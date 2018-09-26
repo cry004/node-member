d87d661e231368ff970a4c2386b35fefbc6ef466
cry004 <cry004@gmail.com>
2018-09-26 15:07:05 +0800

    show message, simple bulletin finish

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ var express = require("express");
 var router = express.Router();
 var firebaseDb = require("../connection/firebase_admin_connect");
 var firebase = require("../connection/firebase_connect");
-router.get("/", function(req, res, next) {
-  var auth = req.session.uid;
-  res.render("index", {
-    title: "Bulletin Board",
-    auth: auth,
-    errors: req.flash('errors')
-  });
+router.get('/', function(req, res, next) {
+  firebaseDb.ref('list').once('value', function(snapshot) {
+    var auth = req.session.uid;
+    res.render('index', {
+      title: "Bulletin Board",
+      auth: auth,
+      errors: req.flash('errors'),
+      list: snapshot.val()
+    });
+  })
 });
 /* GET home page. */
 module.exports = router;
